398c25765a118825108a1c16b46e315f87bf60fc
Abhishek <108579783+Abhishek6543jh@users.noreply.github.com>
2023-10-10 23:37:12 +0530

    j

--- declop/src/components/Navbar.js
+++ declop/src/components/Navbar.js
@@ -2,11 +2,7 @@
 import React from 'react';
 
 const Navbar = () => {
-  return (
-    <nav className="bg-transparent fixed w-full top-0 left-0 right-0 p-4 transition duration-300 ease-in-out">
-      {/* Your navigation code here */}
-    </nav>
-  );
+ 
 };
 
 export default Navbar;
